d31f21857954b5f1af0b8573cedfdece536bde11
nasuda <nasuda908@gmail.com>
2025-12-25 00:40:20 +0900

    add new server

--- dev-server/server.mjs
+++ dev-server/server.mjs
@@ -1,4 +1,15 @@
+import http from 'node:http';
 import fs from "node:fs";
 
 const html = fs.readFileSync("../index.html", "utf-8");
 console.log(html);
+
+
+http
+.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.end('Hello\n');
+})
+.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
